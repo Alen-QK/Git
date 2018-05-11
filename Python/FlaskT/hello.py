dd61ee95ad8d9e0acd9df4efa12f2c86cebe194a
Alen.QK <solaire_sogo@yahoo.com>
2018-05-11 19:01:19 +0800

    2018/5/11

--- Python/FlaskT/hello.py
+++ Python/FlaskT/hello.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 # from datetime import datetime
@@ -6,16 +8,72 @@ from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail
+from flask_mail import Message
+from threading import Thread
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql687610@localhost/test'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+app.config['MAIL_SERVER'] = 'smtp.sina.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <solaire_sogo@sina.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
+db = SQLAlchemy(app)
 bootstarp = Bootstrap(app)
 moment = Moment(app)
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+mail = Mail(app)
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender = app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    roleid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.roleid'))
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/', methods=['GET', 'POST'])
 # def index():
@@ -23,13 +81,19 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)#对应模板中user.username，可以取出用户名
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_mail(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
-
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 # @app.route('/user/<name>')
 # def user(name):
 #     return render_template('user.html', name=name)
@@ -43,4 +107,5 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
+    # manager.run()
     app.run(debug=True)
---
+++ Python/Flasky/config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <solaire_sogo@sina.com>'
+    FALSKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MAIL_SERVER = 'smtp.sina.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    @staticmethod
+    def init_app(app):
+        pass
+    
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql+pymysql://root:*******@localhost/data_dev_mysql'
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'mysql+pymysql://root:*******@localhost/data_test_mysql'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:*******@localhost/data_mysql'
+
+config = {
+    'development' : DevelopmentConfig,
+    'testing' : TestingConfig,
+    'Production' : ProductionConfig,
+
+    'default' : DevelopmentConfig
+}
